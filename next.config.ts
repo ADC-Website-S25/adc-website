2698d446e2b7d7b8a6e449459052d6a5c9850d87
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-08-25 22:36:29 -0400

    fix: fix config basepath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/adc-website' : '';
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: basePath,
+  trailingSlash: true,
 
 };
 

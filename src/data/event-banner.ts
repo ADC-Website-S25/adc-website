c6599f3c79b36f0069335974d3744dba8b552904
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-09-09 16:36:23 -0400

    feat: add first look fair banner

--- src/data/event-banner.ts
+++ src/data/event-banner.ts
@@ -5,32 +5,32 @@ import { Banner } from "@/components/layout/banner";
 // Uncomment the undefined banner to disable the banner
 // Add a banner object to the array to enable a banner at a set time
 
-export const banners: Banner[] = [];
-// export const banners: Banner[] = [
-//   {
-//     title: "App Dev Symposium 2025",
-//     subtitle: "Join us to explore the App Dev Projects and for free pizza!", // Try to keep below 55 characters for 1 line
-//     // href: "/join", // Optional link for the banner title
-//     eventLocation: "Antonov Auditorium, Iribe 0101",
-//     eventDateTime: {
-//       date: new Date('2025-08-06'),
-//       startTime: new Date('2025-08-06T19:00:00'),
-//       endTime: new Date('2025-08-06T20:00:00'),
-//     },
-//     showBannerStartDate: new Date('2025-08-01'),
-//     showBannerEndDate: new Date('2025-08-31'),
-//   },
-//   // {
-//   //   title: "",
-//   //   subtitle: "", // Try to keep below 55 characters for 1 line
-//   //   href: "/",
-//   //   eventLocation: "",
-//   //   eventDateTime: {
-//   //     date: new Date('2025-08-06'),
-//   //     startTime: new Date('2025-08-06T19:00:00'),
-//   //     endTime: new Date('2025-08-06T20:00:00'),
-//   //   },
-//   //   showBannerStartDate: new Date('2025-08-01'),
-//   //   showBannerEndDate: new Date('2025-08-31'),
-//   // },
-// ];
+// export const banners: Banner[] = [];
+export const banners: Banner[] = [
+  {
+    title: "ADC @ the First Look Fair",
+    subtitle: "Stop by our booth to learn more about the club!", // Try to keep below 55 characters for 1 line
+    // href: "/join", // Optional link for the banner title
+    eventLocation: "McKeldin Mall",
+    eventDateTime: {
+      date: new Date('2025-09-12'),
+      startTime: new Date('2025-09-12T10:00:00-04:00'), // 10am EST
+      endTime: new Date('2025-09-12T15:00:00-04:00'),   // 3pm EST
+    },
+    showBannerStartDate: new Date('2025-09-9'),
+    showBannerEndDate: new Date('2025-09-13'),
+  },
+];
+  // {
+  //   title: "",
+  //   subtitle: "", // Try to keep below 55 characters for 1 line
+  //   href: "/",
+  //   eventLocation: "",
+  //   eventDateTime: {
+  //     date: new Date('2025-08-06'),
+  //     startTime: new Date('2025-08-06T19:00:00'),
+  //     endTime: new Date('2025-08-06T20:00:00'),
+  //   },
+  //   showBannerStartDate: new Date('2025-08-01'),
+  //   showBannerEndDate: new Date('2025-08-31'),
+  // },

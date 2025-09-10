b4f28e09861eed5246691a02dd90b49c7211a86a
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-09-09 21:03:16 -0400

    feat: disable apply, internal, and hacks links

--- src/data/links-and-feature-flags.ts
+++ src/data/links-and-feature-flags.ts
@@ -1,8 +1,8 @@
-export const showApplicationLink = true;
+export const showApplicationLink = false;
 export const applicationLink = "https://apply.appdevclub.com/" //TODO: ADD REAL APPLY LINK
 
-export const showInternalLink = true;
+export const showInternalLink = false;
 export const internalLink = "https://corp.appdevclub.com/"
 
-export const showHackathonLink = true;
+export const showHackathonLink = false;
 export const hackathonLink = "https://hackathon.appdevclub.com/" //TODO: ADD REAL HACKATHON LINK

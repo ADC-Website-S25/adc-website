4b230dd0c91d843f91cfc388b851403c622ad552
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-10-15 23:45:24 -0400

    fix: fix spelling

--- src/data/leadership.ts
+++ src/data/leadership.ts
@@ -2,11 +2,27 @@ export type LeadershipMember = {
   name: string;
   role: string;
   src?: string; // Optional photo source
-}
+};
 
 export const leadershipMembers: LeadershipMember[] = [
-  { name: "Atheesh Thirumalairajan", role: "President", src: '/images/leadership/AtheeshThirumalairajan.jpg' },
-  { name: "Amelia Harn", role: "Executive Director", src: '/images/leadership/ameliaHarn.png' },
-  { name: "Hannah Chen", role: "Vice President", src: "/images/leadership/hannahChan.jpg" },
-  { name: "Ashna Nayak", role: "Managing Director", src:"/images/leadership/AshnaNayak.jpg" },
-]
+  {
+    name: "Atheesh Thirumalairajan",
+    role: "President",
+    src: "/images/leadership/AtheeshThirumalairajan.jpg",
+  },
+  {
+    name: "Amelia Harn",
+    role: "Executive Director",
+    src: "/images/leadership/ameliaHarn.png",
+  },
+  {
+    name: "Hannah Chan",
+    role: "Vice President",
+    src: "/images/leadership/hannahChan.jpg",
+  },
+  {
+    name: "Ashna Nayak",
+    role: "Managing Director",
+    src: "/images/leadership/AshnaNayak.jpg",
+  },
+];

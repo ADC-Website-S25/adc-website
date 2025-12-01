5787ddf6918fb7f99d88cd1072e8b121a7ea812b
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-11-30 21:26:13 -0500

    feat: add apply page for exec and director positions

--- src/data/links-and-feature-flags.ts
+++ src/data/links-and-feature-flags.ts
@@ -1,8 +1,9 @@
 export const showApplicationLink = true;
-export const applicationLink = "https://inside.appdevclub.com/" //TODO: ADD REAL APPLY LINK
+// export const applicationLink = "https://inside.appdevclub.com/"
+export const applicationLink = "/apply";
 
 export const showInternalLink = false;
-export const internalLink = "https://corp.appdevclub.com/"
+export const internalLink = "https://corp.appdevclub.com/";
 
 export const showHackathonLink = false;
-export const hackathonLink = "https://hackathon.appdevclub.com/" //TODO: ADD REAL HACKATHON LINK
+export const hackathonLink = "https://hackathon.appdevclub.com/"; //TODO: ADD REAL HACKATHON LINK
